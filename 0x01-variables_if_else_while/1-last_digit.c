10710b7d9f46ae733400459d1b04fd45d25afe6c
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-17 13:44:23 -0700

    The last digit 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,5 @@
 /**
- * main - Prints the last digit of a randomly selected number
- * and whether it is greater than 5, less than 6, or 0
+ * main - Prints the last digit of a randomly selected number and whether it is greater than 5, less than 6, or 0.
  *
  *Return: Always 0.
  */
