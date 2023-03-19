d702f8f7d95a60ad8cd0a57cf5089246adce245a
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-18 23:23:46 -0700

    nsomniA

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet. 
+ * Return: Always (Success)
+*/
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
