8e4912c0883e316f6c60d01b38851846d5ad66ab
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-19 06:53:15 -0700

    4-print_alphabt 3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,10 +14,10 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low; 'a'; low <= 'z'; low++)
-		{
-		if (low != e && low != q)
-		}
+	for ((low 'a'; low <= 'z'); low++)
+	{
+	if (low != e && low != q)
+	}
 	putchar('\n');
 	return (0);
 }
