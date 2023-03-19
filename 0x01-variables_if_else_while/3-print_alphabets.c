1cc7450e45ad0f6199466a5a64edfdc89131dac9
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-19 00:12:24 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - alphaBET
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
