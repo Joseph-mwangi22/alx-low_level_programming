e93af60e68bfac01f9bad6c8036d1cb654f62d20
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-21 13:25:40 -0700

    g sgn in

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_ sign - prints sign of a number
+ * @n: the number to be checked
+ * return: 1 for positive number, -1 for negative number or zero for anything else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}	
