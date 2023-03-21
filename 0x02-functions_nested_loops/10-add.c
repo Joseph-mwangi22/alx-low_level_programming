3970a53f576f73b6f17e61e8443982660f443e7e
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-21 15:05:17 -0700

    fabonnacci

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -ADD two values
+ * @n1: first num to be computed
+ * @n2 second num to be computed
+ * Return: interger value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
