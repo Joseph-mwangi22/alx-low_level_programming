616e745795b71704e358e2c5b48dabbf3ecb5423
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-24 11:03:02 -0700

    isupper 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - check if a letter is upper
- * @x: num to be checked
+ * @c: num to be checked
  * return: 1 for upper letter or 0 for else
  */
 
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
