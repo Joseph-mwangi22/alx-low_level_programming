65a4708bc7c7d72237c05659e88aff863827cb45
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-24 13:10:38 -0700

    amy else

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - check if a letter is upper
  * @c: num to be checked
- * return: 1 for upper letter or 0 for else
+ * return: 1 for upper letter or 0 for any else
  */
 
 int _isupper(int c)
