29e5dbba5aab34ea0ef61df4297667191c40f068
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-27 11:16:23 -0700

    seret

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,11 @@ void rev_string(char *s)
 	char tmp;
 
 	while (s[index++])
-		len++
+		len++;
 	for (index = len - 1; index >= len / 2; index--)
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
