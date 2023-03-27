5b7955ad6e656e52c202110bab217e3c07e29be7
joseph-mwangi22 <wanyagajosephmwangi@gmal.com>
2023-03-27 07:16:35 -0700

    function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a ointer to an int and updates
+ * the value it points to 98
+ *
+ * @n: an integer input pointer
+ *
+ * return : nothing
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * print_numbers - print the numbers since 0 upto 9
+ * @x: character to print
+ * Return: the numbers since 0 upto 9
+ */
+
+int _putchar(char x)
+{
+        return (write(1, &x, 1));
+}
